ba9886a0abcaafc3e9cf91a0f2e9a64aab1796e2
Rayann Nayran <rayannsi@me.com>
2016-09-14 18:25:07 -0300

    Remove login and logout functions from user controller.

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -11,28 +11,7 @@ use MyAcl\Controller\AppController;
 class UsersController extends AppController
 {
 
-    public function login() {
-        $this->viewBuilder()->layout('login');
-        $user = $this->Users->newEntity();
-        if ($this->request->is('post')) {
-            $user = $this->Auth->identify();
-            if ($user) {
-                $this->Auth->setUser($user);
-                return $this->redirect($this->Auth->redirectUrl());
-            }
-            $this->Flash->error(__('Your username or password was incorrect.'));
-        }
-        $this->set(compact('user'));
-        $this->set('_serialize', ['user']);
-    }
-
-    public function logout() {
-        $this->viewBuilder()->layout('login');
-        $this->Flash->success(__('Good-Bye'));
-        $this->redirect($this->Auth->logout());
-    }
-
-        /**
+    /**
      * Index method
      *
      * @return \Cake\Network\Response|null
